3afde5e464c77e98bf0b1048f5afc7be3327f880
Kaodo <46609677+Kaodo@users.noreply.github.com>
2019-06-11 16:01:30 -0700

    Update AddingMachine.java

--- src/cse360assign2/AddingMachine.java
+++ src/cse360assign2/AddingMachine.java
@@ -3,28 +3,36 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0"; //hold the history of transactions
 	}
 	
-	public int getTotal () {
-		return 0;
+	public int getTotal() { //gets total 
+	    return total;
 	}
-	
-	public void add (int value) {
-		
+
+	public void add(int value) { //adds values together
+	    total += value;
+	    history += " + " + value;
 	}
-	
-	public void subtract (int value) {
-		
+
+	public void subtract(int value) { //subtracts values together
+	    total -= value;
+	    history += " - " + value;
+	}
+
+	public String getHistory() { //gets history of transactions that occurred 
+	    return history;
 	}
 		
-	public String toString () {
+	public String toString () { //prints out values
 		return "";
 	}
 
-	public void clear() {
+	public void clear() { //clears out adding machine
 	
 	}
 }
